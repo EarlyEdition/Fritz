fde613929a59ed47638d20906cdb4ee180eeccd0
HorizonEffect <106999252+HorizonEffect@users.noreply.github.com>
2022-07-19 18:38:06 +0430

    Add files via upload

---
+++ users/index.php
@@ -0,0 +1,6 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+  echo '{ "ChatOK": true, "ExpiresInDays": 0, "HighestSubscr": 3, "Result": "OK", "ResultType": 1, "Token": "0000000000000000000000000000", "userId": "ChessCompass" }';
+
+?>
